2dd7f4831b47b57627580160d3bd204e2d5893e2
vasilisgazis <vassilisg2006@gmail.com>
2026-03-20 11:40:59 +0100

    add readme.md and advanced_point.py

--- colorpoint.py
+++ colorpoint.py
@@ -5,6 +5,11 @@ class ColorPoint(Point):
     Color Point class inheriting from Point class
     """
     def __init__(self, x, y, color):
+       #check if x and y are numbers
+       if not isinstance(x,(int,float)): 
+           raise TypeError("x must be a number")
+       if not isinstance(y,(int,float)): 
+           raise TypeError("y must be a number")
         self.x = x
         self.y = y
         self.color = color
